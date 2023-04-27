52b38789113257c018c8572efc3d3cdb4d86040f
nguyenquanghungktmt <nguyenquanghung.ktmt@gmail.com>
2023-04-27 21:27:32 +0700

    feat: add code python engine controller, sensor controller

---
+++ Gas Detection Robot - Python/engines_controller.py
@@ -0,0 +1,99 @@
+import RPi.GPIO as GPIO
+
+# Define hằng số
+MOVE_SPEED = 50
+TURN_SPEED = 60
+MIN_SPEED = 0
+ 
+class EnginesController(object):
+
+ 
+    def __init__(self, left_pwm=12, left_out=16, right_pwm=13, right_out=26):
+        self.left_pwm = left_pwm
+        self.left_out = left_out
+        self.right_pwm = right_pwm
+        self.right_out = right_out
+ 
+ 
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+
+        GPIO.setup(self.left_pwm,GPIO.OUT)
+        GPIO.setup(self.left_out,GPIO.OUT)
+        GPIO.setup(self.right_pwm,GPIO.OUT)
+        GPIO.setup(self.right_out,GPIO.OUT)
+ 
+        # self.stop()
+
+        self.PWMA = GPIO.PWM(self.left_pwm, 500)
+        self.PWMB = GPIO.PWM(self.right_pwm, 500)
+        self.PWMA.start(50)
+        self.PWMB.start(50)
+
+
+    def setPWM_left(self, value):
+        self.PWMA.ChangeDutyCycle(value)
+ 
+    def setPWM_right(self, value):
+        self.PWMB.ChangeDutyCycle(value)  
+ 
+    # Define engine controller functions
+    def move_left(self, speed):
+        self.setPWM_left(speed)
+        # GPIO.output(self.left_pwm, GPIO.HIGH)
+        GPIO.output(self.left_out, GPIO.LOW)
+    
+
+    def move_right(self, speed):
+        self.setPWM_right(speed)
+        # GPIO.output(self.right_pwm, GPIO.HIGH)
+        GPIO.output(self.right_out, GPIO.LOW)
+
+    def stop_left(self):
+        GPIO.output(self.left_pwm, GPIO.LOW)
+        GPIO.output(self.left_out, GPIO.LOW)
+    
+
+    def stop_right(self):
+        GPIO.output(self.right_pwm, GPIO.LOW)
+        GPIO.output(self.right_out, GPIO.LOW)
+    
+
+    def back_left(self, speed):
+        GPIO.output(self.left_out, GPIO.HIGHT)
+        GPIO.output(self.left_pwm, GPIO.LOW)
+
+
+    def back_right(self, speed):
+        GPIO.output(self.right_out, GPIO.HIGHT)
+        GPIO.output(self.right_pwm, GPIO.LOW)
+
+
+    # Define robot controller functions 
+    def move_forward(self):
+        print("move forwward")
+        self.move_left(MOVE_SPEED)
+        self.move_right(MOVE_SPEED)
+    
+
+    def turn_right(self):
+        print("turn right")
+        self.move_right(MIN_SPEED)
+        self.move_left(TURN_SPEED)
+    
+
+    def turn_left(self):
+        print("turn left")
+        self.move_left(MIN_SPEED)
+        self.move_right(TURN_SPEED)
+    
+
+    def move_back(self):
+        self.back_left(MOVE_SPEED)
+        self.back_right(MOVE_SPEED)
+    
+
+    def stop(self):
+        print('stop')
+        self.stop_left()
+        self.stop_right()
---
+++ Gas Detection Robot - Python/ultrasonic_sensors_controller.py
@@ -0,0 +1,66 @@
+import RPi.GPIO as GPIO
+import time
+
+# Define hằng số
+MIN_DISTANCE = 150
+ 
+class UltrasonicSensorsController(object):
+
+    def __init__(self, front_trig=5, front_echo=6, left_trig=22, left_echo=27, right_trig=23, right_echo=24):
+        self.front_trig = front_trig
+        self.front_echo = front_echo
+        self.left_trig = left_trig
+        self.left_echo = left_echo
+        self.right_trig = right_trig
+        self.right_echo = right_echo
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.front_trig,GPIO.OUT)  # Trigger
+        GPIO.setup(self.front_echo,GPIO.IN)   # Echo
+        GPIO.setup(self.left_trig,GPIO.OUT)  # Trigger
+        GPIO.setup(self.left_echo,GPIO.IN)   # Echo
+        GPIO.setup(self.right_trig,GPIO.OUT)  # Trigger
+        GPIO.setup(self.right_echo,GPIO.IN)   # Echo
+
+        GPIO.output(self.front_trig, False)
+        GPIO.output(self.left_trig, False)
+        GPIO.output(self.right_trig, False)
+        time.sleep(0.5)
+
+    # function calculate distanc from sensor to barrier
+    def calculate_distance(self, trigger, echo):
+        # Kích hoạt cảm biến bằng cách ta nháy cho nó tí điện rồi ngắt đi luôn.
+        GPIO.output(trigger, True)
+        time.sleep(0.00001)
+        GPIO.output(trigger, False)
+
+        # Đánh dấu thời điểm bắt đầu
+        start = time.time()
+        while GPIO.input(echo) == 0:
+            start = time.time()
+
+        # Bắt thời điểm nhận được tín hiệu từ Echo
+        stop = 0
+        while GPIO.input(echo)==1:
+            stop = time.time()
+
+        # Thời gian từ lúc gửi tín hiêu
+        elapsed = stop-start
+
+        # Distance pulse travelled in that time is time
+        # multiplied by the speed of sound (cm/s)
+        distance = elapsed * 34300 / 2
+
+        print("Distance : %.1f" % distance)
+        return distance
+    
+    def get_front_distance(self):
+        return self.calculate_distance(self.front_trig, self.front_echo)
+    
+    def get_left_distance(self):
+        return self.calculate_distance(self.left_trig, self.left_echo)
+    
+    def get_right_distance(self):
+        return self.calculate_distance(self.right_trig, self.right_echo)
+
+    
