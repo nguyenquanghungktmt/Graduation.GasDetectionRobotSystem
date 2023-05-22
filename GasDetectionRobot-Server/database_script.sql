a6b488dd2f59e4b886ca98a5cafc9643d22b3f4c
nguyenquanghungktmt <nguyenquanghung.ktmt@gmail.com>
2023-05-22 23:29:55 +0700

    add: file script db, nodejs config connect to db

---
+++ GasDetectionRobot-Server/database_script.sql
@@ -0,0 +1,37 @@
+create table gas_detekt_db;
+
+use gas_detekt_db;
+
+show tables;
+
+create table user (
+	uuid varchar(36) primary key not null,
+    username varchar(100),
+    first_name varchar(100),
+    last_name varchar(100),
+    email varchar(100),
+    password varchar(100),
+    avatar_url varchar(50),
+    created_time datetime,
+    modified_time datetime
+);
+
+create table device (
+	serial_number varchar(10) primary key not null,
+    owner_uuid varchar(36) not null,
+    model_name varchar(100),
+    image_url varchar(50),
+    device_status varchar(100),
+    description text
+);
+
+create table room (
+	room_id varchar(36) primary key not null,
+    room_name varchar(100),
+    owner_uuid varchar(36) not null,
+    is_gas_detect boolean,
+    room_status varchar(100),
+    map2d_url varchar(50),
+    created_time datetime,
+    modified_time datetime
+);
