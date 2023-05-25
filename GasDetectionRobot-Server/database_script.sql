213a015ab528c64edf1fd6d9cf60ce24369d15a3
nguyenquanghungktmt <nguyenquanghung.ktmt@gmail.com>
2023-05-25 17:00:15 +0700

    update file database script

--- GasDetectionRobot-Server/database_script.sql
+++ GasDetectionRobot-Server/database_script.sql
@@ -51,10 +51,10 @@ values ('123e4567-e89b-12d3-a456-426655440000', 'hungnq', '123', 'RB23GT1708');
 
 
 insert into device (serial_number, model_name, description)
-values ('RB23GT1708', 'Gas detection robot', 'Using Raspberry Pi 3, Ultrasonic sensor, gas detect sensor');
+values ('RB23GD1708', 'Gas detection robot', 'Using Raspberry Pi 3, Ultrasonic sensor, gas detect sensor');
 
 insert into device (serial_number, model_name, description)
-values ('RB23GT1807', 'Gas detection robot', 'Using Raspberry Pi 3, Ultrasonic sensor, gas detect sensor');
+values ('RB23GD0002', 'Gas detection robot', 'Using Raspberry Pi 3, Ultrasonic sensor, gas detect sensor');
 
 
 /* select querry */
@@ -66,9 +66,20 @@ SELECT * FROM device WHERE serial_number = "RB23GT1708";
 
 
 select count(*) from user, device
-where user.device_serial_number!='RB23GT1807' and device.serial_number='RB23GT1807';
+where user.device_serial_number!='RB23GT1807' and device.serial_number='RB23GD1807';
 
-SELECT COUNT(*) FROM user JOIN device 
-ON user.device_serial_number!='RB23GT1807' AND device.serial_number='RB23GT1807';
+SELECT COUNT(*) as count FROM user JOIN device 
+ON user.device_serial_number!='RB23GT1708' AND device.serial_number='RB23GD1708';
+
+SELECT COUNT(*) as count FROM user JOIN device 
+ON user.device_serial_number!='RB23GT1807' AND device.serial_number='RB23GD1807';
+
+Select * from device
+where  device.serial_number='RB23GT1807' And not exists (
+	select device_serial_number 
+    from user
+    where user.device_serial_number!='RB23GT1807');
+
+SELECT COUNT(*) as count FROM user WHERE device_serial_number = "RB23GD1708";
 
 
