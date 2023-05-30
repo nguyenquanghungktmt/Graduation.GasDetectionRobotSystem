f6008ead57faa754b389246b0ceec22c7da5778b
nguyenquanghungktmt <nguyenquanghung.ktmt@gmail.com>
2023-05-30 10:15:07 +0700

    update: ripper for room cell

--- gas_detek/lib/widgets/room_widget/room_cell_widget.dart
+++ gas_detek/lib/widgets/room_widget/room_cell_widget.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:gas_detek/constant.dart';
 import 'package:gas_detek/model/room_model.dart';
 
 class RoomCellWidget extends StatelessWidget {
@@ -20,73 +21,82 @@ class RoomCellWidget extends StatelessWidget {
     final colorGasIcon = room.isGasDetect == 1 ? Colors.orange : Colors.blue;
     final textGasStatus =
         room.isGasDetect == 1 ? "Warning! Gas Leak Detection" : "No Gas Leak";
-    // return InkWell(
-    //   child: Container(width: 100, height: 100),
-    //   onTap: () {
-    //     print("Wow! Ripple");
-    //   },
-    // );
-    
-    return InkWell(
-      onLongPress: onLongPress,
-      onTap: onTap,
-      borderRadius: BorderRadius.circular(30),
-      child: Container(
-        decoration: BoxDecoration(
-          color: colorConcept.shade100,
-          borderRadius: const BorderRadius.all(Radius.circular(20.0)),
-          boxShadow: [
-            BoxShadow(color: Colors.grey.withOpacity(0.5),
-              spreadRadius: 5,
-              blurRadius: 7,
-              offset: Offset(0, 3),
-            )
-          ]
+
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(Radius.circular(20.0)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: Offset(0, 3), // changes position of shadow
+          ),
+        ],
+      ),
+      child: Material(
+        color: colorConcept.shade100,
+        borderRadius: const BorderRadius.all(Radius.circular(20.0)),
+        child: InkWell(
+          splashColor: kDarkBlue.withOpacity(0.5),
+          highlightColor: kDarkBlue.withOpacity(0.5),
+          onLongPress: onLongPress,
+          onTap: onTap,
+          borderRadius: BorderRadius.circular(20.0),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(room.roomName,
+                    style: const TextStyle(
+                        fontSize: 18, fontWeight: FontWeight.bold)),
+                Divider(
+                  color: Colors.black,
+                  thickness: 1,
+                ),
+                const SizedBox(height: 10.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.local_fire_department,
+                      size: 24.0,
+                      color: colorGasIcon.shade800,
+                    ),
+                    SizedBox(width: 10),
+                    Text(textGasStatus,
+                      softWrap: false,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 5.0),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.circle,
+                      size: 24.0,
+                      color: colorConcept.shade600,
+                    ),
+                    SizedBox(width: 10),
+                    Text(room.roomStatus,
+                      maxLines: 1,
+                      textDirection: TextDirection.ltr,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+              ],
+            ))
         ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(room.roomName,
-                  style: const TextStyle(
-                      fontSize: 18, fontWeight: FontWeight.bold)),
-              Divider(
-                color: Colors.black,
-                thickness: 1,
-              ),
-              const SizedBox(height: 10.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.local_fire_department,
-                    size: 24.0,
-                    color: colorGasIcon.shade800,
-                  ),
-                  SizedBox(width: 10),
-                  Text(textGasStatus),
-                ],
-              ),
-              const SizedBox(height: 5.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.circle,
-                    size: 24.0,
-                    color: colorConcept.shade600,
-                  ),
-                  SizedBox(width: 10),
-                  Text(room.roomStatus),
-                ],
-              ),
-            ],
-          )),
-      ));
-    
+      ),
+    );
   }
 }
