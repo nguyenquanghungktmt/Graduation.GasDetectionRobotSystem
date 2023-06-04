be1f83a8dbabcaf4e954b71709d7d704a737401f
nguyenquanghungktmt <nguyenquanghung.ktmt@gmail.com>
2023-06-04 23:49:44 +0700

    feat: save avatar inmage in profile screen

--- gas_detek/lib/screens/user_profile_sreen.dart
+++ gas_detek/lib/screens/user_profile_sreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:gallery_saver/gallery_saver.dart';
+import 'package:gas_detek/common/alert_helper.dart';
 import 'package:gas_detek/model/user_model.dart';
 
 import '../constant.dart';
@@ -23,21 +25,27 @@ class _UserProfileState extends State<UserProfile> {
     print(_user.toJson());
   }
 
+  void _saveImage(String url) async {
+    try {
+      await GallerySaver.saveImage(url, albumName: 'Flutter',).then((success) {
+        String message = 'Save failed';
+        if (success ?? false) {
+          message = 'Image saved';
+        }
+        
+        Alert.toastSuccess(message);
+        Alert.closeToast(durationBeforeClose: const Duration(milliseconds: 1500));
+      });
+    } catch (e) {
+      Alert.toastSuccess('Save failed');
+      Alert.closeToast(durationBeforeClose: const Duration(milliseconds: 1500));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     double maxWidth = MediaQuery.of(context).size.width;
-    String avatarUrl = "$domain/images/${_user.avatarUrl}";
-
-    // load avatar
-    Widget avatarImg = Image.network('https://www.w3schools.com/howto/img_avatar.png');
-
-    // try {
-    //   avatarImg = Image.network(avatarUrl);
-    // } on NetworkImageLoadException catch (exception) {
-    //   avatarImg = Icon(Icons.person, color: Colors.grey.shade300);
-    // } catch (error) {
-    //   avatarImg = Icon(Icons.person, color: Colors.grey.shade300);
-    // }
+    String avatarUrl = "$domain/images123/${_user.avatarUrl}";
 
     return Scaffold(
       appBar: AppBar(
@@ -59,53 +67,54 @@ class _UserProfileState extends State<UserProfile> {
         // ),
         actions: [
           PopupMenuButton(
-              icon: const Icon(
-                Icons.more_vert_outlined,
-                size: 36,
-                color: Colors.white,
-              ),
-              itemBuilder: (context) {
-                return [
-                  const PopupMenuItem(
+            icon: const Icon(
+              Icons.more_vert_outlined,
+              size: 36,
+              color: Colors.white,
+            ),
+            itemBuilder: (context) {
+              return [
+                const PopupMenuItem(
+                  padding: EdgeInsets.only(right: 10, left: 20),
+                  value: 0,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Save Avatar'),
+                      SizedBox(width: 5),
+                      Icon(Icons.save_alt_outlined,
+                          size: 20, color: Colors.black87),
+                    ],
+                  ),
+                ),
+                const PopupMenuItem(
                     padding: EdgeInsets.only(right: 10, left: 20),
-                    value: 0,
+                    value: 1,
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Text('Save Avatar'),
+                        Text(
+                          'Delete Account',
+                          style: TextStyle(color: Colors.red),
+                        ),
                         SizedBox(width: 5),
-                        Icon(Icons.save_alt_outlined,
-                            size: 20, color: Colors.black87),
+                        Icon(Icons.delete_rounded, size: 20, color: Colors.red),
                       ],
-                    ),
-                  ),
-                  const PopupMenuItem(
-                      padding: EdgeInsets.only(right: 10, left: 20),
-                      value: 1,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Delete Account',
-                            style: TextStyle(color: Colors.red),
-                          ),
-                          SizedBox(width: 5),
-                          Icon(Icons.delete_rounded,
-                              size: 20, color: Colors.red),
-                        ],
-                      )),
-                ];
-              },
-              onSelected: (value) {
-                switch (value) {
-                  case 0:
-                    print("Save Avatar selected.");
-                    break;
-                  case 1:
-                    print("Delete Room selected.");
-                    break;
-                }
-              }),
+                    )),
+              ];
+            },
+            onSelected: (value) {
+              switch (value) {
+                case 0:
+                  // print("Save Avatar selected.");
+                  _saveImage(avatarUrl);
+                  break;
+                case 1:
+                  print("Delete Room selected.");
+                  break;
+              }
+            }
+          ),
         ],
       ),
       body: SingleChildScrollView(
@@ -116,37 +125,43 @@ class _UserProfileState extends State<UserProfile> {
           children: [
             // Container avatar image
             Container(
-              height: maxWidth * 0.5,
-              width: maxWidth,
-              padding: const EdgeInsets.symmetric(vertical: 24.0),
-              decoration: BoxDecoration(
-                color: kDarkBlue,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.3),
-                    spreadRadius: 2,
-                    blurRadius: 10,
-                    offset: const Offset(2, 2),
-                  ),
-                ],
-              ),
-              child: Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.symmetric(vertical: 10.0),
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.white,
+                height: maxWidth * 0.5,
+                width: maxWidth,
+                padding: const EdgeInsets.symmetric(vertical: 24.0),
+                decoration: BoxDecoration(
+                  color: kDarkBlue,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.3),
+                      spreadRadius: 2,
+                      blurRadius: 10,
+                      offset: const Offset(2, 2),
+                    ),
+                  ],
                 ),
-                child: ClipOval(
-                  child: SizedBox.fromSize(
-                    size: Size.fromRadius(58), // Image radius
-                    child: FittedBox(child: avatarImg, fit: BoxFit.fill),
+                child: Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(vertical: 10.0),
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.white,
                   ),
-                ),
-              )
-            ),
+                  child: ClipOval(
+                    child: SizedBox.fromSize(
+                      size: const Size.fromRadius(58), // Image radius
+                      child: FittedBox(
+                        fit: BoxFit.fitWidth,
+                        child: Image.network(avatarUrl,
+                            errorBuilder: (context, error, stackTrace) {
+                          return Icon(Icons.person,
+                              color: Colors.grey.shade300);
+                        }),
+                      ),
+                    ),
+                  ),
+                )),
             const SizedBox(height: 40.0),
-      
+
             // Username
             Container(
               width: maxWidth - 30.0,
@@ -169,26 +184,28 @@ class _UserProfileState extends State<UserProfile> {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const Text("Username", 
+                    const Text(
+                      "Username",
                       style: TextStyle(
-                        fontSize: 14.0,
-                        fontWeight: FontWeight.w400,
-                        color: Colors.grey
-                      ),
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.grey),
                     ),
-                    Text(_user.username,
+                    Text(
+                      _user.username,
                       style: const TextStyle(
                           fontSize: 16.0,
                           fontWeight: FontWeight.w600,
                           color: Colors.black),
                       maxLines: 1,
-                      overflow: TextOverflow.ellipsis,)
+                      overflow: TextOverflow.ellipsis,
+                    )
                   ],
                 ),
               ),
             ),
             const SizedBox(height: 20.0),
-      
+
             // full name
             Container(
               width: maxWidth - 30.0,
@@ -211,26 +228,28 @@ class _UserProfileState extends State<UserProfile> {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const Text("Fullname", 
+                    const Text(
+                      "Fullname",
                       style: TextStyle(
-                        fontSize: 14.0,
-                        fontWeight: FontWeight.w400,
-                        color: Colors.grey
-                      ),
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.grey),
                     ),
-                    Text(_user.getFullName(),
+                    Text(
+                      _user.getFullName(),
                       style: const TextStyle(
                           fontSize: 16.0,
                           fontWeight: FontWeight.w600,
                           color: Colors.black),
                       maxLines: 1,
-                      overflow: TextOverflow.ellipsis,)
+                      overflow: TextOverflow.ellipsis,
+                    )
                   ],
                 ),
               ),
             ),
             const SizedBox(height: 20.0),
-            
+
             // email
             Container(
               width: maxWidth - 30.0,
@@ -253,26 +272,27 @@ class _UserProfileState extends State<UserProfile> {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const Text("Email", 
+                    const Text(
+                      "Email",
                       style: TextStyle(
-                        fontSize: 14.0,
-                        fontWeight: FontWeight.w400,
-                        color: Colors.grey
-                      ),
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.grey),
                     ),
-                    Text(_user.email,
+                    Text(
+                      _user.email,
                       style: const TextStyle(
                           fontSize: 16.0,
                           fontWeight: FontWeight.w600,
                           color: Colors.black),
                       maxLines: 1,
-                      overflow: TextOverflow.ellipsis,)
+                      overflow: TextOverflow.ellipsis,
+                    )
                   ],
                 ),
               ),
             ),
             const SizedBox(height: 20.0),
-            
           ],
         ),
       ),
