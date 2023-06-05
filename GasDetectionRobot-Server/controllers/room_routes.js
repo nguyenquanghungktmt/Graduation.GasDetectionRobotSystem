ca8a59b33e1101a2a1bea0cdc91110d48f676080
nguyenquanghungktmt <nguyenquanghung.ktmt@gmail.com>
2023-06-06 00:00:26 +0700

    feat: server add api delete room

--- GasDetectionRobot-Server/controllers/room_routes.js
+++ GasDetectionRobot-Server/controllers/room_routes.js
@@ -36,4 +36,31 @@ router.post("/getListRoom", function (req, res) {
   console.log("===========");
 });
 
+// API delete room //
+router.post("/deleteRoom", function (req, res) {
+  console.log("Client request: ", req.body)
+  logger.info(`Client request: deleteRoom - ${JSON.stringify(req.body)}`);
+
+  let room_id = req.body.room_id ?? '';
+  let owner_uuid = req.body.owner_uuid ?? '';
+  var searchQuery = `DELETE FROM room WHERE room_id='${room_id}' AND owner_uuid='${owner_uuid}';`;
+
+
+  const conn = database.createConnection();
+  conn.query(searchQuery, function (err, result) {
+    if (err) {
+      res.status(404).json(response.createResponse(0, 404, "Server Error !"));
+    } else {
+      if(result.affectedRows){
+        res.status(200).json(response.createResponse(1, 200, "Delete Success"));
+      } else {
+        res.status(200).json(response.createResponse(1, 400, "Delete Failed"));
+      }
+    }
+    conn.end();
+  });
+
+  console.log("===========");
+});
+
 module.exports = router;
