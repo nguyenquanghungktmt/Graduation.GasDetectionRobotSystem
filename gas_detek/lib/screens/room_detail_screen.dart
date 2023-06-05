985993050d6bb2449bc9e2248b710019cfe9ddab
nguyenquanghungktmt <nguyenquanghung.ktmt@gmail.com>
2023-06-05 14:07:02 +0700

    feat: add code UI for Room detail screen

--- gas_detek/lib/screens/room_detail_screen.dart
+++ gas_detek/lib/screens/room_detail_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:gallery_saver/gallery_saver.dart';
+import 'package:gas_detek/common/alert_helper.dart';
 import 'package:gas_detek/model/device_model.dart';
 import 'package:gas_detek/model/room_model.dart';
 import 'package:gas_detek/screens/device_info_screen.dart';
@@ -29,12 +31,33 @@ class _RoomDetailState extends State<RoomDetail> {
     final serialNumber = prefs.getString('device_serial_number');
     if (serialNumber != null) {
       DeviceDBHelper.getDevice(serialNumber).then((device) => setState(() {
-        if (device != null) {
-          _device = device;
-          _robotName = device.modelName;
-          _robotSN = device.serialNumber;
+            if (device != null) {
+              _device = device;
+              _robotName = device.modelName;
+              _robotSN = device.serialNumber;
+            }
+          }));
+    }
+  }
+
+  void _saveImage(String url) async {
+    try {
+      await GallerySaver.saveImage(
+        url,
+        albumName: 'Flutter',
+      ).then((success) {
+        String message = 'Save failed';
+        if (success ?? false) {
+          message = 'Image saved';
         }
-      }));
+
+        Alert.toastSuccess(message);
+        Alert.closeToast(
+            durationBeforeClose: const Duration(milliseconds: 1500));
+      });
+    } catch (e) {
+      Alert.toastSuccess('Save failed');
+      Alert.closeToast(durationBeforeClose: const Duration(milliseconds: 1500));
     }
   }
 
@@ -47,6 +70,10 @@ class _RoomDetailState extends State<RoomDetail> {
 
   @override
   Widget build(BuildContext context) {
+    double maxWidth = MediaQuery.of(context).size.width;
+    String map2dUrl = "$domain/images/${_room.map2dUrl}";
+    // String map2dUrl = 'https://raspberrypi.vn/wp-content/uploads/2016/10/raspberry_pi_3.jpg';
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -54,6 +81,8 @@ class _RoomDetailState extends State<RoomDetail> {
           textAlign: TextAlign.center,
           style:
               const TextStyle(color: Colors.white, fontWeight: FontWeight.w700),
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
         ),
         centerTitle: true,
         elevation: 0,
@@ -67,6 +96,14 @@ class _RoomDetailState extends State<RoomDetail> {
           ),
         ),
         actions: [
+          IconButton(
+            onPressed: () { },
+            icon: const Icon(
+              Icons.check_rounded,
+              size: 36,
+              color: Colors.white,
+            ),
+          ),
           PopupMenuButton(
               icon: const Icon(
                 Icons.more_vert_outlined,
@@ -81,7 +118,7 @@ class _RoomDetailState extends State<RoomDetail> {
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Text('Save Image'),
+                        Text('Save Map 2D'),
                         SizedBox(width: 5),
                         Icon(Icons.save_alt_outlined,
                             size: 20, color: Colors.black87),
@@ -109,22 +146,25 @@ class _RoomDetailState extends State<RoomDetail> {
                 switch (value) {
                   case 0:
                     print("Save Image selected.");
+                    _saveImage(map2dUrl);
                     break;
                   case 1:
                     print("Delete Room selected.");
                     break;
                 }
-              }),
+              }
+          ),
         ],
       ),
       body: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 10.0),
+          padding: const EdgeInsets.only(top: 20.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               // create box information about robot
               Container(
+                margin: const EdgeInsets.symmetric(horizontal: 10.0),
                 decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: const BorderRadius.all(Radius.circular(10.0)),
@@ -162,7 +202,9 @@ class _RoomDetailState extends State<RoomDetail> {
                                 crossAxisAlignment: CrossAxisAlignment.center,
                                 children: [
                                   // Text('more infor', style: TextStyle(color: Colors.grey.shade500),),
-                                  Text("more", style: TextStyle(color: Colors.grey.shade500)),
+                                  Text("more",
+                                      style: TextStyle(
+                                          color: Colors.grey.shade500)),
                                   Icon(
                                     Icons.chevron_right_outlined,
                                     color: Colors.grey.shade400,
@@ -171,7 +213,11 @@ class _RoomDetailState extends State<RoomDetail> {
                               ),
                               onTap: () => {
                                 if (_device != null)
-                                  Navigator.push(context, MaterialPageRoute(builder: (context) => DeviceInfo(device: _device!)))
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              DeviceInfo(device: _device!)))
                               },
                             )
                           ]),
@@ -210,8 +256,129 @@ class _RoomDetailState extends State<RoomDetail> {
                   ),
                 ),
               ),
-              const SizedBox(width: 30.0),
+              const SizedBox(height: 30.0),
               // TODO: create main UI below here
+              Container(
+                height: maxWidth,
+                width: maxWidth,
+                margin: const EdgeInsets.symmetric(horizontal: 10.0),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: const BorderRadius.all(Radius.circular(16.0)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.3),
+                      spreadRadius: 2,
+                      blurRadius: 10,
+                      offset: const Offset(2, 2),
+                    ),
+                  ],
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16.0),
+                  child: Image.network(map2dUrl, fit: BoxFit.cover,
+                      errorBuilder: (context, error, stackTrace) {
+                    // return Image.asset('assets/images/icon_404_not_found.png');
+                    return const Text(
+                      "Loading image map ...",
+                      style: TextStyle(fontSize: 24.0),
+                    );
+                  }),
+                ),
+              ),
+              const SizedBox(height: 30.0),
+
+              Expanded(
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: Container(
+                      height: 100.0,
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 16.0, horizontal: 20.0),
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade300,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.3),
+                            spreadRadius: 2,
+                            blurRadius: 10,
+                            offset: const Offset(2, 2),
+                          ),
+                        ],
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: kDarkBlue,
+                              fixedSize: const Size(50, 50),
+                              shape: const CircleBorder(),
+                            ),
+                            child: const Icon(
+                              Icons.play_arrow_rounded,
+                              color: Colors.white,
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: kDarkBlue,
+                              fixedSize: const Size(50, 50),
+                              shape: const CircleBorder(),
+                            ),
+                            child: const Icon(
+                              Icons.pause_rounded,
+                              color: Colors.white,
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: kDarkBlue,
+                              fixedSize: const Size(50, 50),
+                              shape: const CircleBorder(),
+                            ),
+                            child: const Icon(
+                              Icons.stop_rounded,
+                              color: Colors.white,
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: kDarkBlue,
+                              fixedSize: const Size(50, 50),
+                              shape: const CircleBorder(),
+                            ),
+                            child: const Icon(
+                              Icons.add_rounded,
+                              color: Colors.white,
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: kDarkBlue,
+                              fixedSize: const Size(50, 50),
+                              shape: const CircleBorder(),
+                            ),
+                            child: const Icon(
+                              Icons.remove_rounded,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ],
           )),
     );
